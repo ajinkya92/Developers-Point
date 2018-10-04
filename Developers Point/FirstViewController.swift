c46c2d3d1a7dff74b7acd2f8b8df1a26ff33565d
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-10-04 14:03:05 +0530

    Initial Commit

---
+++ Developers Point/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Developers Point
+//
+//  Created by Ajinkya Sonar on 04/10/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Developers Point/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Developers Point
+//
+//  Created by Ajinkya Sonar on 04/10/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
