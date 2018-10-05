729ff10797863347c32983d253eaeff0c9460213
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-10-05 18:24:10 +0530

    ME View Controller Design Complete.

---
+++ Developers Point/Controller/MeVC.swift
@@ -0,0 +1,31 @@
+//
+//  MeVC.swift
+//  Developers Point
+//
+//  Created by Ajinkya Sonar on 05/10/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func signOutButtonTapped(_ sender: UIButton) {
+        
+        //Signout Code
+    }
+    
+    
+    
+    
+}
